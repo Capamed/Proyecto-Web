3ef813fbe6c62f9a9e3927e5ab98bfb9de390eec
Capamed <39675448+Capamed@users.noreply.github.com>
2019-02-06 23:18:12 -0500

    cambios

--- venta-musica/src/tarjeta-credito/tarjeta-credito.controller.ts
+++ venta-musica/src/tarjeta-credito/tarjeta-credito.controller.ts
@@ -4,10 +4,14 @@ import { UsuarioEntity } from "src/usuario/usuario.entity";
 import { CreateAutorDto } from "src/autor/dto/create-autor.dto";
 import { CreateTarjetaCreditoDto } from "./dto/create-tarjetaCredito.dto";
 import { ValidationError, validate } from "class-validator";
+import { TarjetaCreditoService } from "./tarjeta-credito.service";
 
 @Controller('tarjeta-de-credito')
 
 export class TarjetaCreditoController {
+    constructor(private readonly _tarjetaService:TarjetaCreditoService
+    ){
+    }
 
 
     @Get('modo-de-pago/:idUsuario')
@@ -27,15 +31,38 @@ export class TarjetaCreditoController {
         @Body()tarjeta:TarjetaCredito,
         @Param('idUsuario')idUsuario
     ){
-        console.log('sdsdsds',tarjeta)
-        console.log('asdasda',idUsuario)
-        
+    
+        let mensaje = undefined;
+
+        const objetoValidacionTarjeta = new CreateTarjetaCreditoDto();
+
+        objetoValidacionTarjeta.numeroTarjeta = tarjeta.numeroTarjeta
+        objetoValidacionTarjeta.codigoTarjeta = tarjeta.codigoTarjeta
+        objetoValidacionTarjeta.tipoTarjeta = tarjeta.tipoTarjeta
+
+        const errores: ValidationError[] =
+            await validate(objetoValidacionTarjeta) // Me devuelve un arreglo de validacion de errores
+
+        const hayErrores = errores.length > 0;
+
+        if (hayErrores) {
+            console.error(errores)
+
+            const parametrosConsulta = `?error=${errores[0].constraints}`;
+
+            response.redirect('/tarjeta-de-credito/modo-pago' + parametrosConsulta)
+        } else {
+            
+            tarjeta.usuario =idUsuario 
+            const respuesta = await this._tarjetaService.crearTarjeta(tarjeta)
+            response.redirect('/login')
+        }
     }
 
 }
 
 export interface TarjetaCredito{
-    idTarjeta?:string;
+    idTarjeta?:number;
     numeroTarjeta:string;
     codigoTarjeta:number;
     tipoTarjeta: string;
--- venta-musica/src/tarjeta-credito/tarjeta-credito.service.ts
+++ venta-musica/src/tarjeta-credito/tarjeta-credito.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from "@nestjs/common";
 import {TarjetaCreditoEntity} from "./tarjeta-credito.entity";
 import {InjectRepository} from "@nestjs/typeorm";
 import {Repository} from "typeorm";
+import { TarjetaCredito } from "./tarjeta-credito.controller";
 
 @Injectable()
 
@@ -10,7 +11,13 @@ export class TarjetaCreditoService {
         @InjectRepository(TarjetaCreditoEntity)
         private readonly _tarjetaCreditoRepository:Repository<TarjetaCreditoEntity>
     ){
+    }
 
+    async crearTarjeta(nuevaTarjeta:TarjetaCredito): Promise<TarjetaCreditoEntity> {
+        // Instanciar una entidad -> .create()
+        const tarjetaEntity = this._tarjetaCreditoRepository.create(nuevaTarjeta);
+        const tarjetaCreada = await this._tarjetaCreditoRepository.save(tarjetaEntity);
+        return tarjetaCreada;
     }
 
 }
