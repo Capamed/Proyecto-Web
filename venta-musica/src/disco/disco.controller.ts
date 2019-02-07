96aa4e856f96e8717bd24182cf7a94d5f2f0f736
Capamed <39675448+Capamed@users.noreply.github.com>
2019-02-07 05:14:00 -0500

    cambio

--- venta-musica/src/disco/disco.controller.ts
+++ venta-musica/src/disco/disco.controller.ts
@@ -2,6 +2,7 @@ import {Controller, Get, Res, Query, Param} from "@nestjs/common";
 import {DiscoService} from "./disco.service";
 import { DiscoEntity } from "./disco.entity";
 import { FindManyOptions, Like } from "typeorm";
+import { response } from "express";
 
 @Controller('disco')
 
@@ -47,10 +48,38 @@ export class DiscoController {
     }
 
     @Get('detalle-de-disco/:idUsuario')
-    verDisco(
+    async verDisco(
+        @Res()response,
+        @
         @Param('idUsuario')idUsuario,
     ){
-        console.log(idUsuario)
-    }
+        let mensaje = undefined;
 
+        let discos: DiscoEntity[];
+
+        if (busqueda) {
+
+            const consulta: FindManyOptions<DiscoEntity> = {
+                where: [
+                    {
+                        nombreDisco: Like(`%${busqueda}%`)
+                    },
+
+                ]
+            };
+
+            discos = await this._discoService.buscar(consulta);
+        } else {
+
+            discos = await this._discoService.buscar();
+        }
+
+        response.render('catalogo',
+            {
+                arregloDisco: discos,
+                mensaje: mensaje,
+                idUsuario:idUsuario,
+            })
+   
+}
 }
