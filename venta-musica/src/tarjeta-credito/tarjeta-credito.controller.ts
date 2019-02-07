2c7991e29d7c4fce4d0cedd34feec9da1cb5ac1f
Capamed <39675448+Capamed@users.noreply.github.com>
2019-02-06 23:00:57 -0500

    cambios

--- venta-musica/src/tarjeta-credito/tarjeta-credito.controller.ts
+++ venta-musica/src/tarjeta-credito/tarjeta-credito.controller.ts
@@ -1,6 +1,9 @@
 import {Controller, Get, Res, Param, Post, Body} from "@nestjs/common";
 import { response } from "express";
 import { UsuarioEntity } from "src/usuario/usuario.entity";
+import { CreateAutorDto } from "src/autor/dto/create-autor.dto";
+import { CreateTarjetaCreditoDto } from "./dto/create-tarjetaCredito.dto";
+import { ValidationError, validate } from "class-validator";
 
 @Controller('tarjeta-de-credito')
 
@@ -13,16 +16,20 @@ export class TarjetaCreditoController {
         @Param('idUsuario')idUsuario,
     ){
         console.log('estoy',idUsuario)
-        response.render('crear-modoPago')
+        response.render('crear-modoPago',{
+            idUsuario:idUsuario
+        })
     }
 
-    @Post('crear-tarjeta-de-credito')
-    crearMododePago(
+    @Post('crear-tarjeta-de-credito/:idUsuario')
+    async crearMododePago(
         @Res() response,
         @Body()tarjeta:TarjetaCredito,
-
+        @Param('idUsuario')idUsuario
     ){
         console.log(tarjeta)
+        console.log(idUsuario)
+        
     }
 
 }
@@ -30,6 +37,7 @@ export class TarjetaCreditoController {
 export interface TarjetaCredito{
     idTarjeta?:string;
     numeroTarjeta:string;
+    codigoTarjeta:number;
     tipoTarjeta: string;
     usuario: UsuarioEntity;
 }
